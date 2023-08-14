16b742074b0d60bc8712e2968b6bfb7f6f38710b
DevidedDev <108572163+DevidedDev@users.noreply.github.com>
2023-08-14 08:27:28 +0200

    Add basic display input

--- code.js
+++ code.js
@@ -1,6 +1,20 @@
 let num1 = null;
 let num2 = null;
 let operator = null;
+let dispalayValue = "_";
+
+
+const btns = document.querySelectorAll("button");
+btns.forEach((button)=>{
+    button.addEventListener("click", updateDisplay);
+});
+
+const display = {
+    current: document.getElementById("display-current"),
+    result: document.getElementById("dislay-result")
+}
+
+
 
 
 const operations = {
@@ -21,10 +35,16 @@ const operations = {
 }
 
 
+function updateDisplay(){
+    display.current.innerText += this.textContent;
+    console.log("hi")
+}
 
 function operate(num1, operator, num2){
     console.log(operations[operator](num1,num2))
 }
+
+
 
 operate(10, "/", 5);
 
